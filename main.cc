0f83888b613cf39172236af67f5e41e4f8dcc169
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2023-08-11 22:29:48 -0700

    Add class Tokenizer

--- main.cc
+++ main.cc
@@ -2,21 +2,77 @@
 #include <sstream>
 #include <cctype>
 
-static void scan_number(std::istream &strm, int first_char, long &ival)
+enum TokenKind {
+    TOK_UNKNOWN = -1,
+    TOK_INTNUM,
+    TOK_EOF
+};
+
+struct Token {
+    int kind = 0;
+    long ival;
+};
+
+class Tokenizer {
+public:
+    Tokenizer() {}
+    ~Tokenizer() {}
+
+    void SetInput(std::istream &sstrm);
+    int Get(Token &tok);
+
+private:
+    std::istream *strm = nullptr;
+
+    int scan_number(int first_char, Token &tok);
+};
+
+void Tokenizer::SetInput(std::istream &stream)
+{
+    strm = &stream;
+}
+
+int Tokenizer::Get(Token &tok)
+{
+    while (!strm->eof()) {
+        const int ch = strm->get();
+
+        switch (ch) {
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            scan_number(ch, tok);
+            return tok.kind;
+
+        case EOF:
+            tok.kind = TOK_EOF;
+            return tok.kind;
+
+        default:
+            break;
+        }
+    }
+    return TOK_UNKNOWN;
+}
+
+int Tokenizer::scan_number(int first_char, Token &tok)
 {
     static char buf[256] = {'\0'};
     char *pbuf = buf;
 
-    for (int ch = first_char; isdigit(ch); ch = strm.get()) {
+    for (int ch = first_char; isdigit(ch); ch = strm->get()) {
         *pbuf++ = ch;
     }
-    strm.unget();
+    strm->unget();
 
     *pbuf = '\0';
     pbuf = buf;
 
     char *end = NULL;
-    ival = strtol(buf, &end, 10);
+    tok.ival = strtol(buf, &end, 10);
+    tok.kind = TOK_INTNUM;
+
+    return tok.kind;
 }
 
 int main(int argc, char **argv)
@@ -25,12 +81,13 @@ int main(int argc, char **argv)
         return -1;
 
     std::stringstream strm(argv[1]);
+    Tokenizer toknizer;
+    Token tok;
 
-    long ival = 0;
-    int ch = strm.get();
+    toknizer.SetInput(strm);
+    toknizer.Get(tok);
 
-    scan_number(strm, ch, ival);
-    printf("%ld\n", ival);
+    printf("%ld\n", tok.ival);
 
     return 0;
 }
